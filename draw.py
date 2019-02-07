3d8cf0130dce59bcc83d74995ab653b8ce344b21
Maryann <mfoley@stuy.edu>
2019-02-07 11:55:53 -0500

    sort of working

--- draw.py
+++ draw.py
@@ -1,12 +1,27 @@
 from display import *
 
 def draw_line( x0, y0, x1, y1, screen, color ):
+    if x1<x0:
+        x0,x1=x1,x0
+        y0,y1=y1,y0
+    if y1<y0: #pointing downwards
+        if (y1-y0)/(x1-x0) > 1:
+            #octant 7
+            octant=7
+        else:
+            octant=8
+    else:
+        if (y1-y0)/(x1-x0) > 1:
+            #octant 7
+            octant=1
+        else:
+            octant=2
     x=x0
     y=y0
     a=y1-y0
     b=-1*(x1-x0)
     octant=0
-    d2=2*a+b
+    d=2*a+b
     while x<x1:
         plot(screen,color,x,y)
         if d>0:
@@ -14,19 +29,3 @@ def draw_line( x0, y0, x1, y1, screen, color ):
             d+=2*b
         x+=1
         d+=2*a
-    #check which octant
-    if a < 0: #pointing downwards
-        if (y1-y0)/(x1-x0) > 1:
-            #octant 7
-            octant=7
-        else:
-            octant=8
-    else:
-         if (y1-y0)/(x1-x0) > 1:
-             #octant 7
-             octant=1
-         else:
-             octant=2
-
-
-    pass
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from draw import *
 screen = new_screen()
 color = [ 0, 255, 0 ]
 
-draw_line(200,200,100,10,screen,color)
+draw_line(200,10,300,40,screen,color)
+draw_line(300,100,200,40,screen,color)
 display(screen)
 save_extension(screen, 'img.png')
