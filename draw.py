04084d7a469e5ba0da83122da3d73c8d73d93212
Maryann <mfoley@stuy.edu>
2019-02-08 15:30:54 -0500

    tarea

--- draw.py
+++ draw.py
@@ -4,28 +4,47 @@ def draw_line( x0, y0, x1, y1, screen, color ):
     if x1<x0:
         x0,x1=x1,x0
         y0,y1=y1,y0
-    if y1<y0: #pointing downwards
-        if (y1-y0)/(x1-x0) > 1:
+    if y1==y0:
+        if y1<y0:
+            yinc=-1
+            octant=7
+        else:
+            yinc=1
+            octant=1
+    elif y1<y0: #pointing downwards
+        yinc=-1
+        if (y1-y0)/(x1-x0) > -1:
             #octant 7
             octant=7
         else:
             octant=8
     else:
-        if (y1-y0)/(x1-x0) > 1:
-            #octant 7
+        yinc=1
+        if (y1-y0)/(x1-x0) < 1:
+            #octant 1
             octant=1
         else:
             octant=2
+    print(octant)
     x=x0
     y=y0
     a=y1-y0
     b=-1*(x1-x0)
-    octant=0
-    d=2*a+b
-    while x<x1:
-        plot(screen,color,x,y)
-        if d>0:
-            y+=1
+    if octant == 1 or octant == 8:
+        d=2*a*yinc+b
+        while x<x1:
+            plot(screen,color,x,y)
+            if d>0:
+                y+=yinc
+                d+=2*b
+            x+=1
+            d+=2*a*yinc
+    else:
+        d=2*b+a*yinc
+        while y<=y1:
+            plot(screen,color,x,y)
+            if d<0:
+                x+=1
+                d+=2*a
+            y+=yinc
             d+=2*b
-        x+=1
-        d+=2*a
--- main.py
+++ main.py
@@ -6,5 +6,9 @@ color = [ 0, 255, 0 ]
 
 draw_line(200,10,300,40,screen,color)
 draw_line(300,100,200,40,screen,color)
+draw_line(200,200,300,20,screen,color)
+draw_line(200,200,300,200,screen,color)
+draw_line(200,200,300,500,screen,color)
+draw_line(200,200,200,300,screen,color)
 display(screen)
 save_extension(screen, 'img.png')
